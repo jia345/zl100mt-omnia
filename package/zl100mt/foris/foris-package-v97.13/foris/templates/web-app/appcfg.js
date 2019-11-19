69d4d95d8bfad516f8176eeb7ba527d6e457f1eb
cmajor-cd <70248717@qq.com>
2019-11-19 10:53:51 +0800

    V1.0.1 update videoCtr init for swf

--- package/zl100mt/foris/foris-package-v97.13/foris/templates/web-app/appcfg.js
+++ package/zl100mt/foris/foris-package-v97.13/foris/templates/web-app/appcfg.js
@@ -14,7 +14,7 @@ var appCfg = {
     // @param 'type' => define the enable type of the server link for this app
     // @param 'link' => define the multi lines of server link, in case of multi debug/running server data
     debugCfg:{
-        // type: 'mock',   //!!! IMPORTANT, 根据您的需求设置该值. e.g.实际量产时填写 'real'
+        //type: 'mock',   //!!! IMPORTANT, 根据您的需求设置该值. e.g.实际量产时填写 'real'
         type: 'real',   //!!! IMPORTANT, 根据您的需求设置该值. e.g.实际量产时填写 'real'
         link: {         //!!! 请将以下链接填写为自己的真实链接 !!!
             'real': cur_url,
