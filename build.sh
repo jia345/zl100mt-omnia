0be06bd69911233c0d6735341af65b2681f5ed90
JIA Xin <jia345@gmail.com>
2021-12-05 17:41:33 +0800

    fix: remove unnecessary packages to pass building

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-#time CLEAN=1 PKG_BRANCH=stable PRIMARY_ONLY=y ./compile_zl100mt IGNORE_ERRORS=m V=s -j1 2>&1 | tee build.log
-time CLEAN=1 PKG_BRANCH=stable PRIMARY_ONLY=y ./compile_zl100mt V=s -j1 2>&1 | tee build.log
+time CLEAN=1 PKG_BRANCH=stable PRIMARY_ONLY=y ./compile_zl100mt IGNORE_ERRORS=m V=s -j1 2>&1 | tee build.log
+#time CLEAN=1 PKG_BRANCH=stable PRIMARY_ONLY=y ./compile_zl100mt V=s -j1 2>&1 | tee build.log
 #time PKG_BRANCH=stable PRIMARY_ONLY=y ./compile_zl100mt IGNORE_ERRORS=m V=s -j1 2>&1 | tee build.log
 #time SKIP_FEEDS=1 PKG_BRANCH=stable PRIMARY_ONLY=y ./compile_zl100mt IGNORE_ERRORS=m V=s -j1 2>&1 | tee build.log
 #time CLEAN=1 PKG_BRANCH=stable PRIMARY_ONLY=y ./compile_zl100mt V=s -j1 2>&1 | tee build.log
